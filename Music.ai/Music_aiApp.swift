5ccd6a2d1a04c2f4fadaedd8dc9d6d5aaf3ffd45
Karan <karanlokchandani@protonmail.com>
2024-11-12 02:13:46 +0530

    Initial Commit

---
+++ Music.ai/Music_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_aiApp.swift
+//  Music.ai
+//
+//  Created by Karan Haresh Lokchandani on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Music_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
